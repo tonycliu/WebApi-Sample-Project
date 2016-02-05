a2cd61843b5209a4846bcb2b52a15c3682633e90
Tony Liu <tliu@csod.com>
2016-02-04 21:13:48 -0800

    add unit test sample

---
+++ WebApi-Sample-Project.Test/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApi_Sample_Project.Test
+{
+	public class UnitTest
+	{
+		[Fact]
+		public void Test()
+		{
+			Assert.Equal(true, true);
+		}
+	}
+}
